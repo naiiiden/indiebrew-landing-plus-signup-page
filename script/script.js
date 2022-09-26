21adbb838c9056b0783999ecedcfe2a2696772b0
naiiiden <na4ko359@gmail.com>
2022-09-26 22:41:02 +0300

    mobile menu button add aria-* attributes

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
 document.getElementById("hamburger").addEventListener("click", () => {
     document.getElementById("nav-ul").classList.toggle("show");
+    if (document.querySelector("#hamburger").ariaExpanded == "false") {
+        document.querySelector("#hamburger").ariaExpanded = "true";
+        document.querySelector("#hamburger").ariaLabel = "Close menu";
+    } else {
+        document.querySelector("#hamburger").ariaExpanded = "false";
+        document.querySelector("#hamburger").ariaLabel = "Open menu";
+    }
 });
